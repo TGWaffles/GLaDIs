45aeff3289db10b6c5cca0077faed922e1911314
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2023-01-15 01:20:38 +0000

    optional options

--- commands/command.go
+++ commands/command.go
@@ -6,11 +6,11 @@ import (
 )
 
 type ApplicationCommandData struct {
-	Id       discord.Snowflake          `json:"id"`
-	Name     string                     `json:"name"`
-	Type     ApplicationCommandType     `json:"type"`
-	Resolved *interactions.ResolvedData `json:"resolved,omitempty"`
-	Options  []ApplicationCommandOption `json:"options,omitempty"`
-	GuildId  *discord.Snowflake         `json:"guild_id,omitempty"`
-	TargetId *discord.Snowflake         `json:"target_id,omitempty"`
+	Id       discord.Snowflake           `json:"id"`
+	Name     string                      `json:"name"`
+	Type     ApplicationCommandType      `json:"type"`
+	Resolved *interactions.ResolvedData  `json:"resolved,omitempty"`
+	Options  *[]ApplicationCommandOption `json:"options,omitempty"`
+	GuildId  *discord.Snowflake          `json:"guild_id,omitempty"`
+	TargetId *discord.Snowflake          `json:"target_id,omitempty"`
 }
