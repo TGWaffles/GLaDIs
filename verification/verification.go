7d7639f9481dab515e71000acd8ae64d1e724371
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2023-01-19 21:08:00 +0000

    add more information for verification failures

--- verification/verification.go
+++ verification/verification.go
@@ -16,15 +16,21 @@ func VerifyRequest(publicKey string, request events.APIGatewayProxyRequest) bool
 
 	signatureAsString := request.Headers["X-Signature-Ed25519"]
 
-	if signatureAsString == "" {
+	if len(signatureAsString) == 0 {
+		fmt.Println("Missing signature")
 		return false
 	}
 
 	signature, err := hex.DecodeString(signatureAsString)
 	if err != nil {
+		fmt.Println("Failed to decode signature")
 		return false
 	}
 	timestamp := request.Headers["X-Signature-Timestamp"]
+	if len(timestamp) == 0 {
+		fmt.Println("Missing timestamp")
+		return false
+	}
 	body := request.Body
 
 	return ed25519.Verify(key, []byte(timestamp+body), signature)
