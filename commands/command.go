a9795363e021b0f7ee15faa74295d87c88f793e9
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2023-03-17 19:21:26 +0000

    create a getOption() function for command data

--- commands/command.go
+++ commands/command.go
@@ -5,11 +5,31 @@ import (
 )
 
 type ApplicationCommandData struct {
-	Id       discord.Snowflake           `json:"id"`
-	Name     string                      `json:"name"`
-	Type     ApplicationCommandType      `json:"type"`
-	Resolved *discord.ResolvedData       `json:"resolved,omitempty"`
-	Options  *[]ApplicationCommandOption `json:"options,omitempty"`
-	GuildId  *discord.Snowflake          `json:"guild_id,omitempty"`
-	TargetId *discord.Snowflake          `json:"target_id,omitempty"`
+	Id        discord.Snowflake           `json:"id"`
+	Name      string                      `json:"name"`
+	Type      ApplicationCommandType      `json:"type"`
+	Resolved  *discord.ResolvedData       `json:"resolved,omitempty"`
+	Options   *[]ApplicationCommandOption `json:"options,omitempty"`
+	GuildId   *discord.Snowflake          `json:"guild_id,omitempty"`
+	TargetId  *discord.Snowflake          `json:"target_id,omitempty"`
+	optionMap map[string]ApplicationCommandOption
+}
+
+func (commandData *ApplicationCommandData) getOption(optionName string) *ApplicationCommandOption {
+	if commandData.Options == nil {
+		return nil
+	}
+
+	if commandData.optionMap == nil {
+		commandData.optionMap = make(map[string]ApplicationCommandOption)
+		for _, option := range *commandData.Options {
+			commandData.optionMap[option.Name] = option
+		}
+	}
+
+	option, present := commandData.optionMap[optionName]
+	if !present {
+		return nil
+	}
+	return &option
 }
