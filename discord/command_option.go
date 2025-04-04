f325c6fbd17a26f541e01926bf9ee6b82327d95b
Jack Humphries <jackphumphries02@gmail.com>
2025-04-04 17:40:51 +0100

    Added helper functions for retrieving command options
    (cherry picked from commit b7fe4e471683c13145b320f2e69907546c7eb4f2)

--- discord/command_option.go
+++ discord/command_option.go
@@ -3,9 +3,29 @@ package discord
 import "github.com/tgwaffles/gladis/discord/command_option_type"
 
 type ApplicationCommandDataOption struct {
-	Name    string                                `json:"name"`
-	Type    command_option_type.CommandOptionType `json:"type"`
-	Value   interface{}                           `json:"value,omitempty"`
-	Options []ApplicationCommandDataOption        `json:"options,omitempty"`
-	Focused bool                                  `json:"focused,omitempty"`
+	Name      string                                `json:"name"`
+	Type      command_option_type.CommandOptionType `json:"type"`
+	Value     any                                   `json:"value,omitempty"`
+	Options   []ApplicationCommandDataOption        `json:"options,omitempty"`
+	Focused   bool                                  `json:"focused,omitempty"`
+	optionMap map[string]ApplicationCommandDataOption
+}
+
+func (commandDataOption *ApplicationCommandDataOption) GetOption(optionName string) *ApplicationCommandDataOption {
+	if commandDataOption.Options == nil || len(commandDataOption.Options) == 0 {
+		return nil
+	}
+
+	if commandDataOption.optionMap == nil {
+		commandDataOption.optionMap = make(map[string]ApplicationCommandDataOption)
+		for _, option := range commandDataOption.Options {
+			commandDataOption.optionMap[option.Name] = option
+		}
+	}
+
+	option, present := commandDataOption.optionMap[optionName]
+	if !present {
+		return nil
+	}
+	return &option
 }
