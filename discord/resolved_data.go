790917d1ea1eb88fb427e83d2a21662b5576f08d
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2025-07-25 19:27:39 +0100

    add attachments to ResolvedData

--- discord/resolved_data.go
+++ discord/resolved_data.go
@@ -1,8 +1,9 @@
 package discord
 
 type ResolvedData struct {
-	Users    *map[string]*User    `json:"users,omitempty"`
-	Members  *map[string]*Member  `json:"members,omitempty"`
-	Roles    *map[string]*Role    `json:"roles,omitempty"`
-	Channels *map[string]*Channel `json:"channels,omitempty"`
+	Users       *map[string]*User       `json:"users,omitempty"`
+	Members     *map[string]*Member     `json:"members,omitempty"`
+	Roles       *map[string]*Role       `json:"roles,omitempty"`
+	Channels    *map[string]*Channel    `json:"channels,omitempty"`
+	Attachments *map[string]*Attachment `json:"attachments,omitempty"`
 }
